61374231308746418b70c62cd4328cfd023547e0
jjjjj103074 <96659427+jjjjj103074@users.noreply.github.com>
2025-03-04 17:46:55 +0800

    增加linux支援

--- backend/app/utils/Camera.py
+++ backend/app/utils/Camera.py
@@ -3,46 +3,82 @@ import cv2
 import numpy as np
 import time
 import pythoncom
+import platform
 
 from pygrabber.dshow_graph import FilterGraph
-
 from app.models.Camera import Camera, Config
 from config import Camera_Config
 
 class Camera_tool:
 
     def get_all_camera(self):
-        pythoncom.CoInitialize()
-        w = wmi.WMI()
-        graph = FilterGraph()
-
-        devices = graph.get_input_devices()  # 獲取所有可用相機名稱
         camera_info = []
-        for index, name in enumerate(devices):
-            query = "SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%{}%'".format(name)
-            cameraid = w.query(query)
-            camera_id = cameraid[0].DeviceID if cameraid else None
+        system = platform.system()
 
-            camera_info.append(Camera(
-                index = index,
-                name = name,
-                id = camera_id,
-                config = None
-            ))
+        if system == "Windows":
+            pythoncom.CoInitialize()
+            w = wmi.WMI()
+            graph = FilterGraph()
 
-            # 獲取相機設定
-            camera_config = Camera_Config()
-            config = camera_config.get_camera_by_id(camera_id)
-            if config:
-                config = config["config"]
-            if config:
-                camera_info[-1].config = Config(
-                    K = np.array(config["K"]),
-                    postion = np.array(config["postion"]),
-                    orientation = np.array(config["orientation"]),
-                    isenable = config["isenable"]
-                )
+            devices = graph.get_input_devices()  # 獲取所有可用相機名稱
+            for index, name in enumerate(devices):
+                query = "SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%{}%'".format(name)
+                cameraid = w.query(query)
+                camera_id = cameraid[0].DeviceID if cameraid else None
 
+                camera_info.append(Camera(
+                    index=index,
+                    name=name,
+                    id=camera_id,
+                    config=None
+                ))
+
+                # 獲取相機設定
+                camera_config = Camera_Config()
+                config = camera_config.get_camera_by_id(camera_id)
+                if config:
+                    config = config["config"]
+                if config:
+                    camera_info[-1].config = Config(
+                        K=np.array(config["K"]),
+                        postion=np.array(config["postion"]),
+                        orientation=np.array(config["orientation"]),
+                        isenable=config["isenable"]
+                    )
+
+            graph.stop()
+
+        elif system == "Linux":
+            import subprocess
+
+            result = subprocess.run(['v4l2-ctl', '--list-devices'], stdout=subprocess.PIPE)
+            devices = result.stdout.decode().split('\n\n')
+            for index, device in enumerate(devices):
+                lines = device.split('\n')
+                if len(lines) > 1:
+                    name = lines[0]
+                    path = lines[1].strip()
+                    camera_info.append(Camera(
+                        index=index,
+                        name=name,
+                        id=path,
+                        config=None
+                    ))
+
+                    # 獲取相機設定
+                    camera_config = Camera_Config()
+                    config = camera_config.get_camera_by_id(path)
+                    if config:
+                        config = config["config"]
+                    if config:
+                        camera_info[-1].config = Config(
+                            K=np.array(config["K"]),
+                            postion=np.array(config["postion"]),
+                            orientation=np.array(config["orientation"]),
+                            isenable=config["isenable"]
+                        )
+
+        # 添加 ZED 相機
         camera_info.append(Camera(
             index = "zed_left",
             name = "zed_left",
@@ -51,16 +87,14 @@ class Camera_tool:
         ))
 
         camera_info.append(Camera(
-            index = "zed_right",
-            name = "zed_right",
-            id = "zed_right",
-            config = None
+            index="zed_right",
+            name="zed_right",
+            id="zed_right",
+            config=None
         ))
 
-        graph.stop()
-
         return camera_info
-    
+
     def get_camera_by_id(self, id):
         cameras = self.get_all_camera()
         for camera in cameras:
@@ -77,4 +111,4 @@ class Camera_tool:
 
     def dict_to_camera(self, dict):
         return Camera(dict["index"], dict["name"], dict["id"], dict["config"] if "config" in dict else None)
-    
+
