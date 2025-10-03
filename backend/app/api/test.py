ce60d167ef7a0d189fd6722e3890473f1c61552e
jjjjj103074 <96659427+jjjjj103074@users.noreply.github.com>
2025-10-03 12:22:37 +0800

    後端全刪

--- backend/app/api/test.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-test_routes = APIRouter()
-
-@test_routes.get("/hello")
-async def say_hello():
-    return {"message": "Hello, FastAPI!"}
--- backend/app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from threading import Thread
-from .Data_Processing import Data_Processor, Data, Tag, Robot
-data_processor = Data_Processor()
-
-from .Detector import Detector
-detector = Detector()
-
-from .Image_Processing import Image_Processing
-image_processing = Image_Processing()
