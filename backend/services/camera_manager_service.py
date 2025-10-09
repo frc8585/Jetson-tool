dfbd5505c03b15f0a61724320900633e1ef6925f
jjjjj103074 <96659427+jjjjj103074@users.noreply.github.com>
2025-10-09 11:57:04 +0000

    使camera_manager可以處理例外狀況

--- backend/services/camera_manager_service.py
+++ backend/services/camera_manager_service.py
@@ -8,12 +8,21 @@ CAMERA_CONFIG_DIR = os.path.join("backend", "config", "camera.yml")
 #test_camera = Camera(name="Test Camera", camera_id="test_001", backend=0, path="/path/to/test_camera", backend_detail="")
 camera = []
 
+def config_reset():
+    os.makedirs(os.path.dirname(CAMERA_CONFIG_DIR), exist_ok=True)
+    with open(CAMERA_CONFIG_DIR, "w") as f:
+        yaml.safe_dump({"cameras": []}, f)
 
+if not os.path.exists(CAMERA_CONFIG_DIR):
+    config_reset()
 
 def load_camera_config():
     try:
         with open(CAMERA_CONFIG_DIR, "r") as f:
             config = yaml.safe_load(f)
+            if config is None:
+                config_reset()
+                return
             for cam in config.get("cameras", []):
                 if not cam in camera:
                     camera.append(Camera(**cam))
@@ -22,11 +31,15 @@ def load_camera_config():
 
 def save_camera_config():
     try:
+        if not os.path.exists(CAMERA_CONFIG_DIR):
+            config_reset()
         with open(CAMERA_CONFIG_DIR, "w") as f:
             config = {"cameras": [cam.model_dump(mode='json') for cam in camera]}
-            yaml.safe_dump(config, f,)
+            yaml.safe_dump(config, f)
     except Exception as e:
         print(f"Error saving camera config: {e}")
+
+
 
 def get_camera_config():
     return camera
