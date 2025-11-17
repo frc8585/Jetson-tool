9e81f32898e8d614781d29cb7fdcfde69cec08a2
jjjjj103074 <jjjjj103074@gmail.com>
2025-11-17 10:22:27 +0800

    apriltag辨識完成

--- backend/workers/recognition_worker.py
+++ backend/workers/recognition_worker.py
@@ -1,7 +1,9 @@
 
 
 import threading
-import queue
+import queue, cv2
+
+from pupil_apriltags import Detector
 
 from backend.models.camera import CameraImageBuffer
 
@@ -11,16 +13,22 @@ class LocalizationWorker(threading.Thread):
         self.buffer = buffer
         self.running = True
         self.last_frame_id = -1
+        
+        self.detector = Detector(families="tag36h11")
 
     def run(self):
         while self.running:
             # TODO 撰寫影像辨識邏輯
             
-            self.buffer.wait_for_new_frame(self.last_frame_id)
+            fram, timestamp, self.last_frame_id = self.buffer.wait_for_new_frame(self.last_frame_id)
             # TODO 處理取得的影像
-            # 更新 last_frame_id
-            self.last_frame_id += 1
-            print(f"Processed frame id: {self.last_frame_id}")
+            gray = cv2.cvtColor(fram, cv2.COLOR_BGR2GRAY)
+            
+            results = self.detector.detect(gray)
+            for r in results:
+                print(f"Detected tag id: {r.tag_id} at center: {r.center}")
+            
+            # print(f"Processed frame id: {self.last_frame_id}")
 
     def stop(self):
         self.running = False
